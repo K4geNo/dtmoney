2bbef811a90633341c634d48b3179b89eadd8ffa
K4geNo <micaraujo71@gmail.com>
2022-05-15 23:46:59 -0300

    Completing Application

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,54 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createServer, Model } from 'miragejs'
+import { App } from './App'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+createServer({
+    models: {
+        transaction: Model,
+    },
+
+    seeds(server) {
+        server.db.loadData({
+            transactions: [
+                {
+                    id: 1,
+                    title: 'Freelance',
+                    type: 'deposit',
+                    category: 'Dev',
+                    amount: 7000,
+                    createdAt: new Date('2020-01-01'),
+                },
+                {
+                    id: 2,
+                    title: 'Aluguel',
+                    type: 'withdraw',
+                    category: 'Casa',
+                    amount: 1200,
+                    createdAt: new Date('2021-01-01'),
+                },
+            ],
+        })
+    },
+
+    routes() {
+        this.namespace = 'api'
+
+        this.get('/transactions', () => {
+            return this.schema.all('transaction')
+        })
+
+        this.post('/transactions', (schema, request) => {
+            const data = JSON.parse(request.requestBody)
+
+            return schema.create('transaction', data)
+        })
+    },
+})
+
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
